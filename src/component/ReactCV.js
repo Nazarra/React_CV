e249899e2cb9abee4df9b742869d05ebadc52904
Nazarra <bartekdygon@gmail.com>
2020-07-28 12:25:15 +0200

    litte change

--- src/component/ReactCV.js
+++ src/component/ReactCV.js
@@ -12,6 +12,7 @@ const ReactCV = () => {
     return(
 
         <>
+            <div className={'cv_background_container'}>
             <section className={"cv_container"}>
                 <article className={"left_section cv_content"}>
                     <LeftSection />
@@ -21,7 +22,7 @@ const ReactCV = () => {
                 <MiddleSection email={"JanushDEV@gmail.com"}
                                phone={"885445554"}
                                github={"https://github.com/JanushDEV"}
-                               MyName={"Janush Ipsum"}
+                               MyName={"Bartłomiej Dygoń"}
                                YourImage={YourImage}
                                hobby1_icon={"gamepad"}
                                hobby1_description={"Gry komputerowe"}
@@ -43,6 +44,10 @@ const ReactCV = () => {
                 </article>
 
             </section>
+                <p className={'rodo'}>Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji zgodnie z
+                    Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku
+                    z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO)</p>
+            </div>
         </>
     )
 }
